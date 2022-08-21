e6bb37327134e5d6a9bbf206b4659885504ff8d0
Unchaptered <workstation19961002@gmail.com>
2022-08-21 21:14:23 +0900

    Feature Kakao Login 기능 구현 완료

--- src/layers/repositories/user.repository.js
+++ src/layers/repositories/user.repository.js
@@ -1,7 +1,65 @@
+const { User, UserDetail } = require('../../sequelize/models');
+
 /**
  * \# 키워드를 붙이면 해당 클래스의 프로퍼티 안에서 `만` 호출이 가능하다.
  *
  */
-class UserRepository {}
+class UserRepository {
+    constructor() {}
+
+    /**
+     * @param { number } snsTokenId
+     * @returns { Promise<{ userId: number, snsTokenId: number, pinTokenId: number, detailId: number } | null> }
+     */
+    findUserBySnsTokenId = async (snsTokenId) => {
+        const user = await User.findOne({
+            where: {
+                snsTokenId
+            },
+            attributes: ['userId', 'snsTokenId', 'pinTokenId', 'detailId']
+        });
+
+        if (user === null) return null;
+        else {
+            /** @type { { userId: number, snsTokenId: number, pinTokenId: number, detailId: number } } */
+            const findedUser = user.dataValues;
+
+            return findedUser;
+        }
+    };
+
+    /**
+     *
+     * @param { number } snsTokenId
+     * @param { number } pinTokenId
+     * @param { number } detail
+     */
+    uploadUser = async (snsTokenId, pinTokenId, detailId) => {
+        const user = await User.create({
+            snsTokenId,
+            pinTokenId,
+            detailId
+        });
+
+        /** @type { { userId: number, snsTokenId: number, pinTokenId: number, detailId: number } } */
+        const uploadedUser = user.dataValues;
+        return uploadedUser;
+    };
+
+    /**
+     *
+     * @param { string } nickname
+     * @param { string } email
+     * @param { string } ageRnage
+     */
+    uploadUserDetail = async (nickname, email, ageRange) => {
+        const userDetail = await UserDetail.create({ nickname, email, ageRange });
+
+        /** @type { { detailId: number, nickname: string, emial: string, ageRange } } */
+        const uploadedUserDetail = userDetail.dataValues;
+
+        return uploadedUserDetail;
+    };
+}
 
 module.exports = UserRepository;
--- src/layers/services/auth.service.js
+++ src/layers/services/auth.service.js
@@ -1,6 +1,8 @@
 const e = require('express');
 const AuthRepository = require('../repositories/auth.repository');
-const BaseController = require('../controllers/base.controller');
+const UserRepository = require('../repositories/user.repository');
+const { KakaoProvider } = require('../../modules/_.module.loader');
+const { NotFoundException } = require('../../models/_.models.loader');
 
 /**
  * \# 키워드를 붙이면 해당 클래스의 프로퍼티 안에서 `만` 호출이 가능하다.
@@ -8,25 +10,75 @@ const BaseController = require('../controllers/base.controller');
  */
 class AuthService {
     #authRepository;
+    #userRepository;
+    #kakaoProvider;
 
     constructor() {
         this.#authRepository = new AuthRepository();
+        this.#userRepository = new UserRepository();
+        this.#kakaoProvider = new KakaoProvider();
     }
 
     /**
-     * @param { string } token_type
-     * @param { string } access_token
-     * @param { string } expires_in
-     * @param { string } refresh_token
-     * @param { string } refresh_token_expires_in
+     * @param { { accessToken: string, refreshToken: string } } kakaoTokenDto
      */
-    registerAccount = (
-        token_type,
-        access_token,
-        expires_in,
-        refresh_token,
-        refresh_token_expires_in
-    ) => {};
+    registerAccount = async (kakaoTokenDto) => {
+        // true or false
+
+        const userData = await this.#kakaoProvider.getUserDataByKakaoAccessToken(
+            kakaoTokenDto.accessToken
+        );
+
+        const providedId = userData.id;
+
+        // Pin 사이트 용 토큰 등록 절차 실행
+        const accessToken = 'pin access koken';
+        const refreshToken = 'pin refresh token';
+
+        const findedSnsTokenId = await this.#authRepository.findTokenIdByProvidedId(providedId);
+
+        if (findedSnsTokenId === null) {
+            console.log('신규 로그인 가입 절차가 진행됩니다.');
+
+            // 신규 로그인 유저 등록 절차 실행
+            const uploadedSnsToken = await this.#authRepository.uploadSnsToken(
+                providedId,
+                kakaoTokenDto.accessToken,
+                kakaoTokenDto.refreshToken
+            );
+            if (uploadedSnsToken === null) throw new Error('Token 업로드에 실패했습니다.');
+
+            const uploadedPinToken = await this.#authRepository.uploadPinToken(refreshToken);
+
+            // Pin 사이트 용 유저 정보 등록 절차 실행
+            const email = userData.kakao_account.email;
+            const nickname = userData.kakao_account.profile.nickname;
+            const ageRange = userData.kakao_account.age_range;
+            const uploadedUserDetail = await this.#userRepository.uploadUserDetail(
+                nickname,
+                email,
+                ageRange
+            );
+
+            // 최종 User 등록 절차 실행
+            const uploadedUser = await this.#userRepository.uploadUser(
+                uploadedSnsToken.snsTokenId,
+                uploadedPinToken.pinTokenId,
+                uploadedUserDetail.detailId
+            );
+
+            return { accessToken, refreshToken };
+        } else {
+            console.log('기존 회원의 재 로그인 절차가 실행 되었습니다.');
+
+            const findedUser = await this.#userRepository.findUserBySnsTokenId(
+                findedSnsTokenId.snsTokenId
+            );
+            await this.#authRepository.updatePinToken(findedUser.pinTokenId, refreshToken);
+
+            return { accessToken, refreshToken };
+        }
+    };
 
     /**
      * @param { string } refreshToken
