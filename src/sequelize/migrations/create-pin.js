a8225ae5caf3e2db3010a82aecb5cc73bbbf9b17
Unchaptered <workstation19961002@gmail.com>
2022-08-23 18:23:39 +0900

    Bug Fix duplicated key

--- src/sequelize/migrations/create-pin.js
+++ src/sequelize/migrations/create-pin.js
@@ -20,10 +20,6 @@ module.exports = {
                 allowNull: false,
                 type: Sequelize.STRING
             },
-            piCSize: {
-                allowNull: false,
-                type: Sequelize.STRING
-            },
             picSize: {
                 allowNull: false,
                 type: Sequelize.STRING
