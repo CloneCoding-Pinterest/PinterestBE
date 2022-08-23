06e47fa0a6163ee5c2e7afe1b037057387706643
sumin-dev <bonar8888@naver.com>
2022-08-24 00:03:53 +0900

    Refactor fromProvider 이용한 반환 객체 단순화

--- src/layers/services/pin.service.js
+++ src/layers/services/pin.service.js
@@ -1,11 +1,15 @@
 const PinRepository = require('../repositories/pin.repository');
 
 class PinService {
-    pinRepository = new PinRepository();
+    #pinRepository;
+
+    constructor() {
+        this.#pinRepository = new PinRepository();
+    }
 
     //핀 등록
     createPin = async (userId, title, content, picKey, picUrl, picSize) => {
-        const result = await this.pinRepository.createPin(
+        const result = await this.#pinRepository.createPin(
             userId,
             title,
             content,
@@ -19,28 +23,28 @@ class PinService {
 
     //핀 전체 조회
     getPinLists = async (page, count) => {
-        const result = await this.pinRepository.findAllPins(page, count);
+        const result = await this.#pinRepository.findAllPins(page, count);
 
         return result;
     };
 
     //핀 상세 조회
     getPin = async (pinId) => {
-        const result = await this.pinRepository.findPin(pinId);
+        const result = await this.#pinRepository.findPin(pinId);
 
         return result;
     };
 
     //핀 수정
     updatePin = async (pinId, userId, title, content) => {
-        const result = await this.pinRepository.updatePin(pinId, userId, title, content);
+        const result = await this.#pinRepository.updatePin(pinId, userId, title, content);
 
         return result;
     };
 
     //핀 삭제
     deletePin = async (pinId, userId) => {
-        await this.pinRepository.deletePin(pinId, userId);
+        await this.#pinRepository.deletePin(pinId, userId);
 
         return;
     };
